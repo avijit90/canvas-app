974b60da421eb244800717d175fb45d464205f17
avijit90 <sepiaavi@gmail.com>
2019-02-25 15:07:15 +0800

    DrawingService tests updated.

--- src/test/java/com/canvas/app/service/DrawingServiceTest.java
+++ src/test/java/com/canvas/app/service/DrawingServiceTest.java
@@ -1,16 +1,15 @@
 package com.canvas.app.service;
 
 import com.canvas.app.model.Canvas;
-import com.canvas.app.model.Line;
 import com.canvas.app.model.Request;
+import com.canvas.app.model.Shape;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class DrawingServiceTest {
@@ -34,17 +33,34 @@ public class DrawingServiceTest {
     }
 
     @Test
-    public void givenLine_ThenShouldDrawLine() {
-        Request canvasRequest = mock(Request.class);
-        when(canvasRequest.getDimensions()).thenReturn(ImmutableMap.of("x1", 40, "y1", 10));
-        Request lineRequest = mock(Request.class);
-        when(lineRequest.getDimensions()).thenReturn(ImmutableMap.of("x1", 10, "y1", 5, "x2", 20, "y2", 5));
-        Canvas canvas = new Canvas(canvasRequest);
-
-        unit.renderCanvas(canvas);
-        canvas.addShape(new Line(lineRequest));
+    public void givenLineRequest_ThenShouldDrawLine() {
+        Canvas canvas = mock(Canvas.class);
+        Shape shape = mock(Shape.class);
+        canvas.addShape(shape);
 
         unit.drawShapes(canvas);
+
+        verify(shape, times(canvas.getShapes().size())).draw(canvas);
+    }
+
+    @Test
+    public void givenBucketFillRequest_ThenShouldBucketFill() {
+        Canvas canvas = mock(Canvas.class);
+        when(canvas.getWidth()).thenReturn(40);
+        when(canvas.getHeight()).thenReturn(10);
+        when(canvas.getMatrix()).thenReturn(new String[10][40]);
+        Request request = mock(Request.class);
+        when(request.getDimensions()).thenReturn(ImmutableMap.of("x1", 40, "y1", 10));
+        when(request.getColor()).thenReturn("c");
+
+        unit.bucketFill(canvas, request);
+
+        verify(canvas).getWidth();
+        verify(canvas).getHeight();
+        verify(canvas).getMatrix();
+        verify(request, times(2)).getDimensions();
+        verify(request).getColor();
+        verify(canvas).setMatrix(any());
     }
 
 }
